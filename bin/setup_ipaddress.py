a8f861e9f130593e7aede7a4d675a87850179ac7
Doan Bui <bxdoan93@gmail.com>
2021-07-13 16:49:55 +0700

    update config

--- bin/setup_ipaddress.py
+++ bin/setup_ipaddress.py
@@ -3,7 +3,7 @@ import json
 import sys
 import os
 
-LIST_JSON = ['config.test.json', 'config.dev.json']
+LIST_JSON = ['config.json', 'config.dev.json', 'config.test.json']
 sample_json = 'config.sample.json'
 CODE_HOME   = os.path.abspath(os.path.dirname(__file__) + '/..')
 
@@ -13,7 +13,7 @@ def load_file(file_path):
             json_data = json.load(f_r)
             return json_data
     else:
-        print(f'not found {sample_json} - please check it')
+        return None
 
 
 def dump_file(config_data, file_path):
@@ -30,15 +30,18 @@ def setup_address(ip_address=None, json_file=None):
     file_path = f'{CODE_HOME}/{json_file}'
     print(f'Update ipaddress file {json_file}')
     config_json_data = load_file(file_path)
-    config_data = copy.deepcopy(config_json_data)
-    print(f"[BEFORE] postgres : {config_data['postgres']}")
-    config_data['postgres'].update({
-        "host": ip_address
-    })
-    print(f"[AFTER]  postgres : {config_data['postgres']}")
-    config_data = upd_more_config(config_data)
-    dump_file(config_data=config_data, file_path=file_path)
-    print(f'Update ipaddress file {json_file}...Done!')
+    if config_json_data:
+        config_data = copy.deepcopy(config_json_data)
+        print(f"[BEFORE] postgres : {config_data['postgres']}")
+        config_data['postgres'].update({
+            "host": ip_address
+        })
+        print(f"[AFTER]  postgres : {config_data['postgres']}")
+        config_data = upd_more_config(config_data)
+        dump_file(config_data=config_data, file_path=file_path)
+        print(f'Update ipaddress file {json_file}...Done!')
+    else:
+        print(f'not found {file_path} - please check it')
 
 
 def upd_more_config(config_data):
@@ -47,18 +50,39 @@ def upd_more_config(config_data):
         with open(file_path) as f_r:
             config_json_data = json.load(f_r)
             config_sample_data = copy.deepcopy(config_json_data)
-            for key, value in config_sample_data.items():
-                value_by_key = config_data.get(key)
-                if not value_by_key:
-                    print(f"[ADD MORE]  {key} : {value}")
-                    config_data.update({
-                        key: value
-                    })
-    else:print(f'not found {sample_json} - please check it')
-
+            config_data = upd_val_key(config_sample_data, config_data)
+    else:
+        print(f'not found {sample_json} - please check it')
     return config_data
 
 
+def upd_val_key(sample_dict, real_dict):
+    for key, value_sample_by_key in sample_dict.items():
+        if not value_sample_by_key:
+            continue
+        elif isinstance(value_sample_by_key, dict):
+            value_real_by_key = real_dict.get(key)
+            if isinstance(value_real_by_key, dict):
+                val_after = upd_val_key(value_sample_by_key, value_real_by_key)
+                real_dict = do_change_val(real_dict, key, val_after, f_print=False)
+            else:
+                real_dict = do_change_val(real_dict, key, value_sample_by_key)
+        else:
+            value_real_by_key = real_dict.get(key)
+            if not value_real_by_key:
+                real_dict = do_change_val(real_dict, key, value_sample_by_key)
+    return real_dict
+
+
+def do_change_val(data, key, value, f_print=True):
+    if f_print:
+        print(f"[ADD MORE]  {key} : {value}")
+    data.update({
+        key: value
+    })
+    return data
+
+
 if __name__ == '__main__':
-    ip_address = sys.argv[1]
-    setup_list_config_file(ip_address)
+    ip_add = sys.argv[1]
+    setup_list_config_file(ip_add)
