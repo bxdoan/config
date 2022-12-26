abb1e3068eea8187d0b34d7801189caee9d6e36c
Doan Bui <bxdoan93@gmail.com>
2022-12-26 16:05:39 +0700

    get pipenv application

--- bin/setipaddress.sh
+++ bin/setipaddress.sh
@@ -3,14 +3,18 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 pg="gc_postgres_dn"
 to="$1"
-if [[ $OSTYPE == 'darwin'* ]]; then
-  pipenv="/opt/homebrew/bin/pipenv"
-  your_ip="localhost"
-else
-  your_ip=$(docker inspect ${pg} | grep -oP '(?<="IPAddress": ")[^"]*' | head -n1)
-  [ -x pipenv ] && pipenv='pipenv' || pipenv="$HOME/.pyenv/shims/pipenv"
-fi
+your_ip=$(docker inspect ${pg} | grep -oP '(?<="IPAddress": ")[^"]*' | head -n1)
 echo "Your IP ${pg} is ${your_ip}"
+if [[ -f "$HOME/.pyenv/shims/pipenv" ]]; then
+  [ -x pipenv ] && pipenv='pipenv' || pipenv="$HOME/.pyenv/shims/pipenv"
+elif [[ -f "$HOME/.local/bin/pipenv" ]]; then
+  [ -x pipenv ] && pipenv='pipenv' || pipenv="$HOME/.local/bin/pipenv"
+elif [[ -f "/opt/homebrew/bin/pipenv" ]]; then
+  pipenv="/opt/homebrew/bin/pipenv"
+else
+  echo "pipenv application not found"
+  exit 1
+fi
 value="$your_ip;$to"
 
 _() {
