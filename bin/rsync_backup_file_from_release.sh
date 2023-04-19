283fb3ef858fc0fc5f6978c7cc75ca8c2cb9ebc4
Doan Bui <bxdoan93@gmail.com>
2023-04-19 16:03:16 +0700

    rsync

--- bin/rsync_backup_file_from_release.sh
+++ bin/rsync_backup_file_from_release.sh
@@ -2,6 +2,7 @@
 # run on release to have backup file: docker exec phoenix_db_c pg_dump -Ujarvis -d phoenix | gzip -c > /tmp/hoa/phoenix_20230202_1156.gz
 #./script/tool_script/rsync_backup_file_from_release.sh /tmp/don/ phoenix_file.gz
 
+timestamp=$(date +%Y%m%d_%H%M)
 # download file from release to local
 # if param is phoenix, then use phoenix_db_c
 # if param is lens, then use lens_c
@@ -25,7 +26,7 @@ fi
 
 file=$(basename ${filepath})
 echo $file
-echo  "rsync -e ssh jingr@release.s.gigacover.com:$filepath ~/Downloads/$file"
+echo "rsync -e ssh jingr@release.s.gigacover.com:$filepath ~/Downloads/$file"
 rsync -e ssh jingr@release.s.gigacover.com:$filepath ~/Downloads/$file
 
 # remove .gz in file
@@ -33,24 +34,41 @@ f="${file/.gz/}"
 echo "$f from $file"
 
 # unzip first
-gzip -d ~/Downloads/$file
+echo "gzip -df ~/Downloads/$file"
+gzip -df ~/Downloads/$file
 
 # restore to db
+
 # remember create database "root" and role "gc_writable" "jarvis" first
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE root;"
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE ROLE gc_writable;"
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE ROLE jarvis;"
 if [ "$1" = "phoenix" ]; then
+  echo "DROP DATABASE phoenix;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE phoenix;"
+
+  echo "CREATE DATABASE phoenix;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE phoenix;"
+
+  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d phoenix"
   cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d phoenix
 elif [ "$1" = "lens" ]; then
+  echo "DROP DATABASE lens_m;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE lens_m;"
+
+  echo "CREATE DATABASE lens_m;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE lens_m;"
+
+  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d lens_m"
   cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d lens_m
 elif [ "$1" = "swift" ]; then
+  echo "DROP DATABASE swift_m;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE swift_m;"
+
+  echo "CREATE DATABASE swift_m;"
   docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE swift_m;"
+
+  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d swift_m"
   cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d swift_m
 elif [ "$1" = "don" ]; then
   echo "Do nothing"
