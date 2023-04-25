4785b310e121f48c1690467153edf04bf1093ed3
Doan Bui <bxdoan93@gmail.com>
2023-04-25 18:28:58 +0700

    optimize code

--- bin/rsync_backup_file_from_release.sh
+++ bin/rsync_backup_file_from_release.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
 # usage
-# run on release to have backup file: docker exec phoenix_db_c pg_dump -Ujarvis -d phoenix | gzip -c > /tmp/hoa/phoenix_20230202_1156.gz
-#./script/tool_script/rsync_backup_file_from_release.sh /tmp/don/ phoenix_file.gz
+#./bin/rsync_backup_file_from_release.sh /tmp/don/ phoenix_file.gz
 
+EC='\033[0m'    # end coloring
+HL='\033[0;33m' # high-lighted color
+ER='\033[0;31m' # red color
+CM='\033[0;32m' # comment color
+GR='\033[0;32m' # green color
+WH='\033[0;37m' # white color
 STARTTIME=$(date +%s)
 function print_exe_time() {
     STARTTIME=${1}
@@ -11,30 +17,28 @@ function print_exe_time() {
 It takes ${GR}%s${EC} seconds to complete this script...\n" " ${EXE_TIME}"
 }
 # download file from release to local
-# if param is phoenix, then use phoenix_db_c
-# if param is lens, then use lens_c
-# if param is swift, then use swift_c
-# if param is don, then use don_c
+don_dir=/tmp/don
+
+POSTGRES_USER='root'
+CONTAINER_NAME="$1_c"
+DATABASE_NAME="$1_m"
 if [ "$1" = "phoenix" ]; then
   POSTGRES_USER='postgres'
   CONTAINER_NAME="phoenix_db_c"
-  filepath=/tmp/don/phoenix_file.gz
-elif [ "$1" = "lens" ]; then
-  POSTGRES_USER='root'
-  CONTAINER_NAME="lens_c"
-  filepath=/tmp/don/lens_file.gz
-elif [ "$1" = "swift" ]; then
-  POSTGRES_USER='root'
-  CONTAINER_NAME="swift_c"
-  filepath=/tmp/don/swift_file.gz
-elif [ "$1" = "don" ]; then
-  filepath=/tmp/don/phoenix_file.gz
+  DATABASE_NAME='phoenix'
 fi
 
+filepath="$don_dir/$1_file.gz"
 file=$(basename ${filepath})
 echo $file
 
 if [ "$2" = "-rsync" ]; then
+  ssh jingr@release.s.gigacover.com "mkdir -p $don_dir"
+
+  cmd_dump="docker exec $CONTAINER_NAME pg_dump -U jarvis -d $DATABASE_NAME | gzip -c > $filepath"
+  echo "ssh jingr@release.s.gigacover.com $cmd_dump"
+  ssh jingr@release.s.gigacover.com $cmd_dump
+
   echo "rsync -e ssh jingr@release.s.gigacover.com:$filepath ~/Downloads/$file"
   rsync -e ssh jingr@release.s.gigacover.com:$filepath ~/Downloads/$file
 fi
@@ -53,35 +57,13 @@ gzip -df ~/Downloads/$file
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE root;"
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE ROLE gc_writable;"
 #docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE ROLE jarvis;"
-if [ "$1" = "phoenix" ]; then
-  echo "DROP DATABASE phoenix;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE phoenix;"
+echo "DROP DATABASE $DATABASE_NAME;"
+docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE $DATABASE_NAME;"
 
-  echo "CREATE DATABASE phoenix;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE phoenix;"
+echo "CREATE DATABASE $DATABASE_NAME;"
+docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE $DATABASE_NAME;"
 
-  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d phoenix"
-  cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d phoenix
-elif [ "$1" = "lens" ]; then
-  echo "DROP DATABASE lens_m;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE lens_m;"
-
-  echo "CREATE DATABASE lens_m;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE lens_m;"
-
-  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d lens_m"
-  cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d lens_m
-elif [ "$1" = "swift" ]; then
-  echo "DROP DATABASE swift_m;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "DROP DATABASE swift_m;"
-
-  echo "CREATE DATABASE swift_m;"
-  docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c "CREATE DATABASE swift_m;"
-
-  echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d swift_m"
-  cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d swift_m
-elif [ "$1" = "don" ]; then
-  echo "Do nothing"
-fi
+echo "cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d $DATABASE_NAME"
+cat ~/Downloads/$f | docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d $DATABASE_NAME
 
 print_exe_time "${STARTTIME}"
